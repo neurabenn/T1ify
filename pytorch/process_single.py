191e3ac55968e03c553398af43eff877f20f9d13
Marian <mail@marian42.de>
2020-04-07 22:48:15 +0200

    Add a script to process a single file

---
+++ pytorch/process_single.py
@@ -0,0 +1,36 @@
+from itertools import count
+from skimage import io
+
+import torch
+from torch.utils.data import DataLoader
+from jpeg_dataset import JpegDataset
+from tqdm import tqdm
+from torchvision import utils
+import math
+
+from unet import UNet
+import sys
+
+input_filename = sys.argv[1]
+output_filename = sys.argv[2]
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+NETWORK_FILENAME = 'trained_models/unet.to'
+network = UNet()
+network.load_state_dict(torch.load(NETWORK_FILENAME))
+network.cuda()
+
+
+image = io.imread(input_filename)[:, :, 0] / 255.0
+SIZE_BASE = 64
+size = tuple(math.ceil(x / SIZE_BASE) * SIZE_BASE for x in image.shape)
+
+tensor = torch.zeros(size, dtype=torch.float32, device=device)
+tensor[:image.shape[0], :image.shape[1]] = torch.tensor(image, dtype=torch.float32, device=device)
+
+
+with torch.no_grad():
+    output = network(tensor.unsqueeze(0)).squeeze().squeeze()
+
+output = output[:image.shape[0], :image.shape[1]]
+utils.save_image(output, output_filename)
