9bd7fa508aac143cc3eaade5aae0a80073f72a8c
stefanches7 <dvoretsk@in.tum.de>
2020-04-08 12:35:23 +0300

    cherry pick actual .nii data from hpc in another directory

--- hpc_t1w_t2w_brain_download.sh
+++ hpc_t1w_t2w_brain_download.sh
@@ -2,6 +2,7 @@
 
 # random samples to download
 SAMPLES=${1-10}
+SUBJECTS_LIST=${1-available_subjects_hcp_t1t2.txt}
 
 if [ ! -d "hcp_openacess" ]; then
 	datalad install ///hcp-openaccess 
@@ -13,7 +14,15 @@ cd hcp-openaccess
 #sort random on subdirectories of HCP1200 (which stand for different samples data
 ls HCP1200 |sort -R |tail -$SAMPLES | while read sample; do
 	# add subject to downloaded
-	echo "$sample" >> ../available_subjects_hcp_t1t2.txt
+	echo "$sample" >> ../$SUBJECTS_LIST
 	datalad get HCP1200/$sample/T1w/T1w_acpc_dc_restore_brain.nii.gz
 	datalad get HCP1200/$sample/T1w/T2w_acpc_dc_restore_brain.nii.gz	
 done
+
+cd ..
+while read sample; do
+	mkdir -p hcp-openaccess-t1t2/HCP1200/$sample/T1w
+	cp -L hcp-openaccess/HCP1200/$sample/T1w/T1w_acpc_dc_restore_brain.nii.gz hcp-openaccess-t1t2/HCP1200/$sample/T1w/
+	cp -L hcp-openaccess/HCP1200/$sample/T1w/T2w_acpc_dc_restore_brain.nii.gz hcp-openaccess-t1t2/HCP1200/$sample/T1w/
+done < $SUBJECTS_LIST
+
